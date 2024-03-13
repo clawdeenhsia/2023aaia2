f9647391e4cde12403a149f5157fe9c38de6e3bb
clawdeenhsia <91250836+clawdeenhsia@users.noreply.github.com>
2024-03-13 12:00:07 +0800

    Add files via upload

---
+++ week04-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        total = 0
+        for i in range(1, n+1):
+            total += i
+
+
+    left = 0
+    for i in range(1,n+1):
+        lrft += i
+        if left + left - i == total:
+    return -1
